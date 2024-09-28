36fcc379cf77778a10d73f6eef217aaf08422ff2
Juan <jbellevue83022@gmail.com>
2024-09-28 00:00:03 -0500

    Added the logic to complete a round

--- script.js
+++ script.js
@@ -26,10 +26,54 @@ function getHumanChoice() {
 }
 
 function playRound(getHumanChoice, getComputerChoice) {
-getComputerChoice();
-getHumanChoice();
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+lose = () => {computerScore++; alert("You lost!\n Computer Score: " + computerScore + "\nYour score: " + playerScore);}
+win = () => {playerScore++; alert("You Won!\n Computer Score: " + computerScore + "\nYour score: " + playerScore);}
+tie = () => alert("You Tied!\n Computer Score: " + computerScore + "\nYour score: " + playerScore);
+
+alert("Human chose " + humanChoice)
+alert("Computer chose " + computerChoice)
+if (humanChoice == "Rock" && computerChoice == "Paper")
+{
+    lose();
+}
+else if (humanChoice == "Paper" && computerChoice == "Rock")
+{
+    win();
+}
+else if (humanChoice == "Paper" && computerChoice == "Scissors")
+{
+        lose();   
+}
+else if (humanChoice == "Scissors" && computerChoice == "Paper")
+ {
+            win();
+ }  
+else if (humanChoice == "Rock" && computerChoice == "Scissors")
+{
+        win();
+}
+else if (humanChoice == "Scissors" && computerChoice == "Rock")
+{
+            lose();
+}
+else if (humanChoice == "Rock" && computerChoice == "Rock")
+{
+                tie();
+}
+else if (humanChoice == "Paper" && computerChoice == "Paper")
+{
+                    tie();
+}
+else if (humanChoice == "Scissors" && computerChoice == "Scissors")
+    {
+                        tie();
+    }
+
+
 }
 
-playRound()
+playRound(getHumanChoice, getComputerChoice)
 
 
